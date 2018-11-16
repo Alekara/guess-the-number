1f11d5e29cf6e00861901171f8ecfbe2a0503719
Alekara <43556614+Alekara@users.noreply.github.com>
2018-11-17 01:02:23 +0300

    причесала код, вынесла все повторы в отдельные функции

--- guess.js
+++ guess.js
@@ -7,15 +7,10 @@ var screen1 = document.querySelector(".screen1");
 var screen2 = document.querySelector(".screen2");
 
 var theAnswer = document.querySelector(".answer");
-
 var theCounter = document.querySelector(".tryNumber");
 
-var theGuess = 5000;
-var lastMore = 0;
-var lastLess = 10000;
-var counter = 1;
-
 btnStart.addEventListener("click", function () {
+  resetStats();
   screen1.classList.add("hide");
   screen2.classList.remove("hide");	
 	theAnswer.textContent = "Это число 5000?";
@@ -30,52 +25,50 @@ btnExact.addEventListener("click", function () {
 btnMore.addEventListener("click", checkMore);
 btnLess.addEventListener("click", checkLess);
 
-function checkMore () {
+function checkMore() {
   lastMore = theGuess;
-	theGuess = Math.round(lastMore + (lastLess - lastMore) / 2);
-  counter += 1;
-  theCounter.textContent = counter;
-  theAnswer.textContent = " Это число " + theGuess + "?";
-  if (counter > 14) {
-    theAnswer.textContent = "Упс! Как это возможно? Вы точно где-то ошиблись...";
-    resetButtons();
-    btnAgain.addEventListener("click", resetGame);
-  }
-  return counter;
+  newGuess();
 };
 
-function checkLess () {
+function checkLess() {
   lastLess = theGuess;
-	theGuess = Math.round(lastMore + (lastLess - lastMore)/2);
-  counter += 1;
-  theCounter.textContent = counter;
-  theAnswer.textContent = " Это число " + theGuess + "?";
-  if (counter > 14) {
-    theAnswer.textContent = "Упс! Как это возможно? Вы точно где-то ошиблись...";
-    resetButtons();
-    btnAgain.addEventListener("click", resetGame);
-  }
-  return counter;
+  newGuess();
 };
   
-  function resetGame() {
-    screen1.classList.remove("hide");
-    screen2.classList.add("hide");
-    theGuess = 5000;
-    lastMore = 0;
-    lastLess = 10000;
-    counter = 1;
-    theCounter.textContent = counter;
-    btnMore.style.display = "block";
-    btnLess.style.display = "block";
-    btnExact.style.display = "block";
-    btnAgain.style.display = "none";
-  }
+function resetGame() {
+  screen1.classList.remove("hide");
+  screen2.classList.add("hide");
+  resetStats();
+  theCounter.textContent = counter;
+  btnMore.style.display = "block";
+  btnLess.style.display = "block";
+  btnExact.style.display = "block";
+  btnAgain.style.display = "none";
+}
 
 function resetButtons() {
   btnMore.style.display = "none";
   btnLess.style.display = "none";
   btnExact.style.display = "none";
   btnAgain.style.display = "block";
+}
+
+function resetStats() {
+  theGuess = 5000;
+  lastMore = 0;
+  lastLess = 10000;
+  counter = 1;
+}
+
+function newGuess() {
+  theGuess = Math.round(lastMore + (lastLess - lastMore)/2);
+  counter += 1;
+  theCounter.textContent = counter;
+  theAnswer.textContent = " Это число " + theGuess + "?";
+  if (counter > 14) {
+    theAnswer.textContent = "Упс! Как это возможно? Вы точно где-то ошиблись...";
+    resetButtons();
+    btnAgain.addEventListener("click", resetGame);
+  }
 }
 
